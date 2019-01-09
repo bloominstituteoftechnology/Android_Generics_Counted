e765469a638a18ecb4b27d3c6ad463827291866e
JTyzz <jtyzzer@gmail.com>
2019-01-09 15:50:33 -0800

    CountedSet java class completed.

--- src/CountedSet.java
+++ src/CountedSet.java
@@ -1,6 +1,28 @@
 import java.util.ArrayList;
 
 public class CountedSet<T> {
-    ArrayList<T> data = new ArrayList<>();
+    ArrayList<T> data;
 
+    public CountedSet() {
+        this.data = new ArrayList<>();
+    }
+
+    public void insert(T item) {
+        data.add(item);
+    }
+
+    public void remove(T item) {
+        data.remove(item);
+    }
+
+    public int count(T item) {
+        int amount = 0;
+
+        for(T object : data) {
+            if(object.equals(item)) {
+                amount++;
+            }
+        }
+        return amount;
+    }
 }
