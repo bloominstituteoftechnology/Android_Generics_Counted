2d629eba27bec69bc75852636050bec83e62dbcb
JTyzz <jtyzzer@gmail.com>
2019-01-09 16:12:32 -0800

    Added test cases to main, changed array type from String to Object.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        CountedSet<Object> set = new CountedSet<>();
+
+        //test tests
+        set.insert("test");
+        set.insert("test");
+        set.insert("test");
+        set.insert("test");
+        set.insert("test");
+        //5
+        System.out.println(set.count("test"));
+        set.remove("test");
+        set.remove("test");
+        //3
+        System.out.println(set.count("test"));
+
+        set.insert("");
+        set.insert("");
+        set.insert("");
+        set.insert("");
+        //4
+        System.out.println(set.count(""));
+
+        set.insert(true);
+        set.insert(true);
+        set.insert(false);
+        //2 1
+        System.out.println(set.count(true)+" "+ set.count(false));
+
     }
 }
