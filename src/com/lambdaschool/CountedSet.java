8bf6fb0fa576888cafc7831c35045d14ed40dadd
heiligbasil <basilief@yahoo.com>
2019-04-24 16:01:43 -0700

    Added CountedSet generics class and got the test code in Main to work

---
+++ src/com/lambdaschool/CountedSet.java
@@ -0,0 +1,53 @@
+package com.lambdaschool;
+
+public class CountedSet<GenericType> {
+
+    private GenericType[] elementArray;
+    //private int elementLengthIndex;
+
+    public CountedSet() {
+        this.elementArray = (GenericType[]) new Object[0];
+        //this.elementLengthIndex = 0;
+    }
+
+    public void insert(GenericType elementToInsert) {
+        GenericType[] elementArrayCopy = (GenericType[]) new Object[this.elementArray.length + 1];
+
+        for (int i = 0; i < this.elementArray.length; ++i) {
+            elementArrayCopy[i] = this.elementArray[i];
+        }
+        this.elementArray = (GenericType[]) new Object[elementArrayCopy.length];
+        this.elementArray = elementArrayCopy;
+        this.elementArray[this.elementArray.length - 1] = elementToInsert;
+    }
+
+    public void remove(GenericType elementToRemove) {
+        for (int i = 0; i < this.elementArray.length; ++i) {
+            if (this.elementArray[i].equals(elementToRemove)) {
+                GenericType[] elementArrayCopy = (GenericType[]) new Object[this.elementArray.length - 1];
+                int index = 0;
+
+                for (int j = 0; j < this.elementArray.length; ++j) {
+                    if (i != j) {
+                        elementArrayCopy[index++] = this.elementArray[j];
+                    }
+                }
+                this.elementArray = (GenericType[]) new Object[elementArrayCopy.length];
+                this.elementArray = elementArrayCopy;
+                break;
+            }
+        }
+    }
+
+    public int count(String textToFindAndCountIterationsOf) {
+        int count = 0;
+
+        for (int i = 0; i < this.elementArray.length; ++i) {
+            if (this.elementArray[i].equals(textToFindAndCountIterationsOf)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
--- src/com/lambdaschool/Main.java
+++ src/com/lambdaschool/Main.java
@@ -1,8 +1,20 @@
 package com.lambdaschool;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Test cases
+        CountedSet<String> set = new CountedSet<>();
+        System.out.println(set.count("test")); // 0
+        set.insert("test");
+        set.insert("test");
+        System.out.println(set.count("test")); // 2
+        set.remove("test");
+        set.remove("test");
+        System.out.println(set.count("test")); //0
+
+
     }
 }
